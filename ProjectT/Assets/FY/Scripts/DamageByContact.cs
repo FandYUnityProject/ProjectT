f2131eb5190fb5f28188b32f43ae69ca6aede326
whitewhale6 <whitewhale6@gmail.com>
2015-08-12 08:23:14 +0900

    EnemyのNavテスト

---
+++ ProjectT/Assets/FY/Scripts/DamageByContact.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageByContact : MonoBehaviour {
+
+	public int amountDamage;
+	public float waitTime = 0.5f;
+	public float Timer;
+
+	void Update(){
+		Timer += Time.deltaTime;
+	}
+
+	void OnTriggerEnter(Collider coll){
+		if (coll.gameObject.tag == "Player" && Timer > waitTime) {
+			//PlayerHealth.TakeDamage(amountDamege);
+			Timer = 0f;
+		}
+	}
+}
---
+++ ProjectT/Assets/FY/Scripts/EnemyHealth.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour {
+
+	public int health;
+
+	void OnCollisionEnter (Collision coll){
+		if (coll.gameObject.tag == "BallWeapon") {
+			TakeDamage();
+		}
+	}
+
+	void TakeDamage(){
+		health -= 1;
+		if (health <= 0) {
+			Destroy(gameObject,5f);
+		}
+	}
+}
