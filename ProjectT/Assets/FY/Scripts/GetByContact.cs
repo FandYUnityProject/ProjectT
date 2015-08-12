d09113b5614f3a4ec7942303bf1878d93ca837ef
whitewhale6 <whitewhale6@gmail.com>
2015-08-12 16:23:06 +0900

    星型item（スコア用）の作成

---
+++ ProjectT/Assets/FY/Scripts/GetByContact.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetByContact : MonoBehaviour {
+	public AudioSource av;
+
+	void OnCollisionEnter(Collision coll){
+		if (coll.gameObject.tag == "Player" || coll.gameObject.tag == "BallWeapon") {
+			//process get items and reflect to pose scene
+			//UIかPlayerのScriptの関数を呼び出す形で連携
+			av.Play ();
+			if (!av.isPlaying)
+				Destroy (gameObject);
+		}
+	}
+}
---
+++ ProjectT/Assets/FY/Scripts/ItemRotateController.cs
@@ -0,0 +1,17 @@
+﻿/*
+ *itemをその場でy軸中心に回転させる
+ * 2015/8/12 - Fujita
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class ItemRotateController : MonoBehaviour {
+
+	public float rotationalSpeed;
+
+	void Update(){
+		transform.Rotate (new Vector3 (0f, rotationalSpeed, 0f));
+	}
+
+}
