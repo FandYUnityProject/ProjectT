45bf47778ed6f6ccd29dc979a75b8372647fb5f3
whitewhale6 <whitewhale6@gmail.com>
2015-08-16 11:13:55 +0900

    Enemy作成

--- ProjectT/Assets/FY/Scripts/EnemyMoveControllerChase.cs
+++ ProjectT/Assets/FY/Scripts/EnemyMoveControllerChase.cs
@@ -5,13 +5,32 @@ public class EnemyMoveControllerChase : MonoBehaviour {
 
 	Transform player;
 	NavMeshAgent nav;
+	Animator anim;
+	bool isDetect = false;
 
 	void Awake(){
 		player = GameObject.FindGameObjectWithTag ("Player").transform;
 		nav = GetComponent<NavMeshAgent> ();
+		anim = GetComponent<Animator> ();
 	}
 
 	void Update(){
-		nav.SetDestination (player.position);
+		if (isDetect) {
+			anim.SetBool ("Walking", true);
+			nav.SetDestination (player.position);
+		} else {
+			anim.SetBool ("Walking", false);
+		}
+	}
+
+	void OnTriggerEnter(Collider coll){
+		if (coll.gameObject.tag == "Player") {	
+			isDetect =true;
+		}
+	}
+	void OnTriggerExit(Collider coll){
+		if (coll.gameObject.tag == "Player") {	
+			isDetect =false;
+		}
 	}
 }
