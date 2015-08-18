2ae8fbde3823a573c8c16f53f2eda63a795b5818
whitewhale6 <whitewhale6@gmail.com>
2015-08-18 11:29:01 +0900

    PlanetGravityScriptの作成

---
+++ ProjectT/Assets/FY/Scripts/PlanetGravityMover.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetGravityMover : MonoBehaviour {
+
+	private Rigidbody rb;
+	public Vector3 localGravity;
+
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void Update(){
+		AddGravity();
+	}
+
+	void OnTriggerStay(Collider coll){
+		if (coll.gameObject.tag == "Planet") {
+			localGravity = PlanetGravity.GravityCalc(transform.position);
+		}
+	}
+
+	void AddGravity(){
+		rb.AddForce (localGravity);
+	}
+}
