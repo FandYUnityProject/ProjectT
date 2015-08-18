d4c05c12ac9f1e359f2bebdd6dbf776a6fb3b0bd
Guttyon3 <y.yamaguchi0329@gmail.com>
2015-08-18 21:09:14 +0900

    【MAPS】SkyboxをJOJOに変化できるようにする。

--- ProjectT/Assets/YY/Scripts/SkyBoxRotateChange.cs
+++ ProjectT/Assets/YY/Scripts/SkyBoxRotateChange.cs
@@ -3,40 +3,47 @@ using System.Collections;
 
 public class SkyBoxRotateChange : MonoBehaviour {
 	
-	public Material MorningSkyBox;
-	public Material NightSkyBox;
+	public Material SkyBox;
 	
-	public float duration  = 3.0f;	// Skyboxを徐々に変化させる時間
+	public float duration  = 1.0f;	// Skyboxを徐々に変化させる時間
 	public float skyRotate = 0.01f; // Skyboxを回転させる早さ
-
+	
 	// Use this for initialization
 	void Start () {
 	
-		RenderSettings.skybox = MorningSkyBox;
+		// スカイボックスをセットし、shaderのBlendの値を0(朝）に設定。
+		RenderSettings.skybox = SkyBox;
+		SkyBox.SetFloat("_Blend", 0.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
+		// スカイボックスをを回転させる
 		transform.Rotate (0.0f, skyRotate, 0.0f);
-		//float lerp = Mathf.PingPong(Time.time, duration) / duration;
 	}
+
+	private void UpdateHandler(float value)
+	{
+		SkyBox.SetFloat("_Blend", value);
+	}
+
 
 	void OnTriggerEnter(Collider coll){
 
 		if (coll.gameObject.name == "Player") {
-			Debug.Log("IN");
-			RenderSettings.skybox = NightSkyBox;
-			//RenderSettings.skybox.Lerp(MorningSkyBox, NightSkyBox, lerp);
+
+			// 朝から夜に変化
+			iTween.ValueTo(gameObject, iTween.Hash("from", 0.0f, "to", 1.0f, "time", duration, "onupdate", "UpdateHandler"));
 		}
 	}
 
 	void OnTriggerExit(Collider coll){
 		
 		if (coll.gameObject.name == "Player") {
-			Debug.Log("OUT");
-			RenderSettings.skybox = MorningSkyBox;
-			//RenderSettings.skybox.Lerp(NightSkyBox, MorningSkyBox, lerp);
+
+			// 夜から朝に変化
+			iTween.ValueTo(gameObject, iTween.Hash("from", 1.0f, "to", 0.0f, "time", duration, "onupdate", "UpdateHandler"));
 		}
 	}
 }
