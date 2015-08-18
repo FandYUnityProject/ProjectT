0e9e7b70f8040959e8eeb1307cd4a513bcf5aa60
whitewhale6 <whitewhale6@gmail.com>
2015-08-18 17:44:23 +0900

    発狂アゲ

---
+++ ProjectT/Assets/FY/Scripts/CameraTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTest : MonoBehaviour {
+
+	Vector3 playerOffset;
+	Vector3 planetOffset;
+	Vector3 viewPoint;
+	GameObject player;
+	GameObject planet;
+	Transform target;
+	static float angle = 0f;//= Vector3.Angle (playerOffset, target.position);
+	static float radius = 15f;
+
+	void Start(){
+		player = GameObject.FindGameObjectWithTag ("Player");
+		planet = GameObject.FindGameObjectWithTag ("Planet");
+		target= player.transform;
+		playerOffset = transform.position - player.transform.position;
+		planetOffset = transform.position - planet.transform.position;
+	}
+
+	void FixedUpdate (){
+
+		//Debug.Log (angle);
+		//transform.position = player.transform.position + planetOffset;
+		//transform.LookAt (planet.transform);
+	
+		//transform.rotation = Quaternion.AngleAxis (angle,Vector3.right);
+	}
+}
