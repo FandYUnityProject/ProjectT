a8a17763692481313ba664ebf0a6b73c5a1066c2
Guttyon3 <y.yamaguchi0329@gmail.com>
2015-08-20 21:45:43 +0900

    【MAPS】AltキーによるSkyboxの回転を作成

--- ProjectT/Assets/YY/Scripts/SkyBoxRotateChange.cs
+++ ProjectT/Assets/YY/Scripts/SkyBoxRotateChange.cs
@@ -5,8 +5,15 @@ public class SkyBoxRotateChange : MonoBehaviour {
 	
 	public Material SkyBox;
 	
-	public float duration  = 1.0f;	// Skyboxを徐々に変化させる時間
-	public float skyRotate = 0.01f; // Skyboxを回転させる早さ
+	public float duration;	 // Skyboxを徐々に変化させる時間
+	public float skyRotate ; // Skyboxを回転させる早さ
+	
+	public float smooth  = 1.5f;	// カメラモーションのスムーズ化用変数
+	public Transform skyCamPosNormal;	// the usual position for the camera, specified by a transform in the game
+	public Transform skyCamPosUp;		// Jump Camera locater
+	
+	// スムーズに繋がない時（クイック切り替え）用のブーリアンフラグ
+	bool bQuickSwitch = false;	//Change Camera Position Quickly
 	
 	// Use this for initialization
 	void Start () {
@@ -18,12 +25,64 @@ public class SkyBoxRotateChange : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-		// スカイボックスをを回転させる
-		transform.Rotate (0.0f, skyRotate, 0.0f);
+
 	}
 
-	private void UpdateHandler(float value)
+	void FixedUpdate ()	// このカメラ切り替えはFixedUpdate()内でないと正常に動かない
+	{
+		//Alt
+		if(Input.GetButton("Fire2")){	
+			// Change Jump Camera
+			setCameraPositionJumpView();
+		} else {	
+			// return the camera to standard position and direction
+			setCameraPositionNormalView();
+		}
+
+		
+		// スカイボックスをを回転させる
+		skyCamPosNormal.Rotate (0.0f, skyRotate, 0.0f);
+		skyCamPosUp.Rotate (0.0f, skyRotate, 0.0f);
+
+		/*
+		if (Input.GetButtonDown ("Fire2")) {
+			isAltKey = true;
+			iTween.RotateTo (gameObject, iTween.Hash ("x", -60.0f, "time", smooth));
+		}
+		
+		if (Input.GetButtonUp ("Fire2")) {
+			isAltKey = false;
+			iTween.RotateTo (gameObject, iTween.Hash ("x",  0.0f, "time", smooth));
+		}
+		*/
+	}
+
+	void setCameraPositionNormalView()
+	{
+		if(bQuickSwitch == false){
+			// the camera to standard position and direction
+			transform.position = Vector3.Lerp(transform.position, skyCamPosNormal.position, Time.fixedDeltaTime * smooth);	
+			transform.forward = Vector3.Lerp(transform.forward, skyCamPosNormal.forward, Time.fixedDeltaTime * smooth);
+		}
+		else{
+			// the camera to standard position and direction / Quick Change
+			transform.position = skyCamPosNormal.position;	
+			transform.forward = skyCamPosNormal.forward;
+			bQuickSwitch = false;
+		}
+	}
+	
+	void setCameraPositionJumpView()
+	{
+		// Change Jump Camera
+		bQuickSwitch = false;
+		transform.position = Vector3.Lerp(transform.position, skyCamPosUp.position, Time.fixedDeltaTime * smooth);	
+		transform.forward = Vector3.Lerp(transform.forward, skyCamPosUp.forward, Time.fixedDeltaTime * smooth);		
+	}
+
+
+
+	private void UpdateSky(float value)
 	{
 		SkyBox.SetFloat("_Blend", value);
 	}
@@ -34,7 +93,7 @@ public class SkyBoxRotateChange : MonoBehaviour {
 		if (coll.gameObject.name == "Player") {
 
 			// 朝から夜に変化
-			iTween.ValueTo(gameObject, iTween.Hash("from", 0.0f, "to", 1.0f, "time", duration, "onupdate", "UpdateHandler"));
+			iTween.ValueTo(gameObject, iTween.Hash("from", 0.0f, "to", 1.0f, "time", duration, "onupdate", "UpdateSky"));
 		}
 	}
 
@@ -43,7 +102,7 @@ public class SkyBoxRotateChange : MonoBehaviour {
 		if (coll.gameObject.name == "Player") {
 
 			// 夜から朝に変化
-			iTween.ValueTo(gameObject, iTween.Hash("from", 1.0f, "to", 0.0f, "time", duration, "onupdate", "UpdateHandler"));
+			iTween.ValueTo(gameObject, iTween.Hash("from", 1.0f, "to", 0.0f, "time", duration, "onupdate", "UpdateSky"));
 		}
 	}
 }
